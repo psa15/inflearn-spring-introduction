17edf61ad482db4dd89ddb014f74953312238f6f
sua <ssuuaa.park@gmail.com>
2022-10-29 21:14:15 +0900

    회원 도메인과 리포지토리

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+        //람다식 문법
+        //member에서 받아온 이름이 파라미터의 이름이 같으면 하나라도 반환하라는 의미
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
