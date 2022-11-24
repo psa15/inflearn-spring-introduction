0a88c4b90c2c7d8821193c0c6a038febcba4b410
sua <ssuuaa.park@gmail.com>
2022-11-24 20:55:19 +0900

    h2설정 및 순수JDB

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,22 +1,33 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 //@Service나 @Autowired 등을 사용하지 않고(컴포넌트 스캔을 사용하지 않고) 빈 설정하기
 //자바 코드로 직접 스프링 빈 등록하기
 @Configuration
 public class SpringConfig {
 
-/*    @Bean
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }*/
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
 }
