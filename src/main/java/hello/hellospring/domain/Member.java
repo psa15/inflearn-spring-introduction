2bcc3382f4c02167b18dd45fa231d1195a9d7168
sua <ssuuaa.park@gmail.com>
2022-11-24 22:31:36 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,11 +1,41 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
 //데이터 : 회원 ID, 이름
 //기능 : 회원 등록, 조회
 //아직 데이터 저자오가 선정되지 않음(가상의 시나리오) -> 회원리포지토리를 인터페이스로 구현(가벼운 메모리 기반의 저장소에 데이터 저장)
-public class Member {
+/*public class Member {
     public String name;
     private Long id;
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}*/
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //시퀀스처럼 db에서 자동으로 설정해주는 것 = IDENTITY
+    private Long id;
+
+    //@Column(name = "username") //db와 이름이 다를 경우
+    public String name;
 
     public Long getId() {
 
--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,36 +1,46 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 //@Service나 @Autowired 등을 사용하지 않고(컴포넌트 스캔을 사용하지 않고) 빈 설정하기
 //자바 코드로 직접 스프링 빈 등록하기
 @Configuration
 public class SpringConfig {
-
+/*
     private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+    */
+
+    //jpa
+    private EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+
     @Bean
-    public MemberService memberService(){
+
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepository memberRepository(){
+    public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
